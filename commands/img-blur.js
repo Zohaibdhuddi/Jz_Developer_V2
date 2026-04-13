5b1270cf1f7b03ef6e0e87e6868da5b0bd04f262
@ZOHAIB_ABBAS <161803896+Zohaibdhuddi@users.noreply.github.com>
2026-04-13 20:14:27 +0500

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029Vb7QERSHVvTgzwBBU41M@newsletter',
+                    newsletterName: '@ZOHAIB_ABBAS',
                     serverMessageId: -1
                 }
             }
